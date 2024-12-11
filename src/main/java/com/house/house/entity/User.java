2d728be2eb2dbc4dd5574f137e3c90fc30401619
parker0509 <167055018+parker0509@users.noreply.github.com>
2024-12-11 19:48:56 +0900

    contiroom v.0.0.1(DB plus)

--- src/main/java/com/house/house/entity/User.java
+++ src/main/java/com/house/house/entity/User.java
@@ -3,7 +3,7 @@ package com.house.house.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 
 
 @Entity
